9960a9fd3107c9a5d1c863e7d5c7d9b4b41452ed
UnixxSH <17497055+UnixxSH@users.noreply.github.com>
2021-06-27 12:24:35 +0200

    add table view, buttons and show them

--- app/views.py
+++ app/views.py
@@ -9,18 +9,43 @@
 
 ## Modules
 from PyQt5.QtWidgets import (
+    QAbstractItemView,
     QHBoxLayout,
     QMainWindow,
+    QPushButton,
+    QTableView,
+    QVBoxLayout,
     QWidget,
 )
 
 ## Define main window
 class Window(QMainWindow):
+    ## Show main window
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.setWindowTitle("RP Contacts")
+        self.setWindowTitle("RPcontact")
         self.resize(550, 250)
         self.centralWidget = QWidget()
         self.setCentralWidget(self.centralWidget)
         self.layout = QHBoxLayout()
         self.centralWidget.setLayout(self.layout)
+        ## Call for window setup
+        self.setupUI()
+    ## Window items setup
+    def setupUI(self):
+        # Create table view widget
+        self.table = QTableView()
+        self.table.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.table.resizeColumnsToContents()
+        # Create buttons
+        self.addButton = QPushButton("Add...")
+        self.deleteButton = QPushButton("Delete")
+        self.clearAllButton = QPushButton("Clear All")
+        # Lay out the GUI
+        layout = QVBoxLayout()
+        layout.addWidget(self.addButton)
+        layout.addWidget(self.deleteButton)
+        layout.addStretch()
+        layout.addWidget(self.clearAllButton)
+        self.layout.addWidget(self.table)
+        self.layout.addLayout(layout)
