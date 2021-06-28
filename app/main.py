40931573d7244a409980d3db5ab50cbdd94081ae
UnixxSH <17497055+UnixxSH@users.noreply.github.com>
2021-06-28 16:25:13 +0200

    add database connection check

--- app/main.py
+++ app/main.py
@@ -11,10 +11,17 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from .views import Window
+from .database import createConnection
 
 ## Show main window
 def main():
+    ## Create application
     app = QApplication(sys.argv)
+    ## Initilize database connection
+    if not createConnection("contacts.sqlite"):
+        sys.exit(1)
+    ## Create window
     win = Window()
     win.show()
+    ## Run event loop
     sys.exit(app.exec())
